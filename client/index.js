e67cd4e30285060c47e1a0778c97daa54fca0ae6
Jeremiah Arella <jeremiah.arella@gmail.com>
2016-12-12 01:33:51 -0800

    PIXI test render

--- client/index.js
+++ client/index.js
@@ -5,3 +5,21 @@ var board = new Board(6,7);
 
 var renderer = new PIXI.WebGLRenderer(800,600);
 document.body.appendChild(renderer.view);
+var stage = new PIXI.Container();
+var mario = null;
+
+PIXI.loader.add('bunny', './assets/images/mario.png').load(function (loader, resources) {
+  mario = new PIXI.Sprite(resources.bunny.texture);
+  mario.position.x = 400;
+  mario.position.y = 300;
+  mario.scale.x = .1;
+  mario.scale.y = .1;
+  stage.addChild(mario);
+  animate();
+});
+
+var animate = function() {
+  requestAnimationFrame(animate);
+  mario.rotation += 0.01;
+  renderer.render(stage);
+}
