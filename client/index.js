1569de9cd26deab220c68f8b41a5b8f381a20f24
Jeremiah Arella <jeremiah.arella@gmail.com>
2016-12-12 03:56:20 -0800

    Sprite falls when clicked

--- client/index.js
+++ client/index.js
@@ -4,22 +4,37 @@ var PIXI = require('pixi.js');
 var board = new Board(6,7);
 
 var renderer = new PIXI.WebGLRenderer(800,600);
+console.log(renderer.plugins.interaction);
 document.body.appendChild(renderer.view);
 var stage = new PIXI.Container();
 var mario = null;
 
-PIXI.loader.add('bunny', './assets/images/mario.png').load(function (loader, resources) {
-  mario = new PIXI.Sprite(resources.bunny.texture);
-  mario.position.x = 400;
-  mario.position.y = 300;
+PIXI.loader.add('mario', './assets/images/mario.png').load(function (loader, resources) {
+  mario = new PIXI.Sprite(resources.mario.texture);
+
+  mario.position.y = 10;
+  renderer.plugins.interaction.on('mousemove',(data) => {
+    var mouse = data.data.global;
+    mario.position.x = mouse.x;
+  });
+
+  renderer.plugins.interaction.on('mousedown', (data) => {
+    fall();
+    console.log('click');
+  });
+
   mario.scale.x = .1;
   mario.scale.y = .1;
   stage.addChild(mario);
   animate();
 });
 
+var fall = function() {
+  mario.position.y += 10;
+  requestAnimationFrame(fall);
+}
+
 var animate = function() {
   requestAnimationFrame(animate);
-  mario.rotation += 0.01;
   renderer.render(stage);
 }
