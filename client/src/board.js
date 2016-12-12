88c7f9bf095d1c1aea422c6a7defa939ff8c960f
Jeremiah Arella <jeremiah.arella@gmail.com>
2016-12-11 19:46:52 -0800

    Add script tag for board source code

--- client/src/board.js
+++ client/src/board.js
@@ -2,5 +2,5 @@ var Board = function(columns, rows) {
   this.slots = {};
   this.currentPlayer = 'red';
   this.winner = null;
-  if (columns && rows) { this.build(columns, rows) }; 
+  if (columns && rows) { this.build(columns, rows) };
 }
