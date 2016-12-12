9af3c3d55f8fc6566f278d8dec0b508e547377cd
Jeremiah Arella <jeremiah.arella@gmail.com>
2016-12-12 01:28:58 -0800

    Renders PIXI onto DOM

--- client/index.js
+++ client/index.js
@@ -2,4 +2,6 @@ import Board from './src/board';
 var PIXI = require('pixi.js');
 
 var board = new Board(6,7);
-console.log(PIXI);
+
+var renderer = new PIXI.WebGLRenderer(800,600);
+document.body.appendChild(renderer.view);
